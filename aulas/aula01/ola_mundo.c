047107564df60d2011f9c0a7139989585594c223
Guilherme conti <guilherme.conti@iesb.edu.br>
2025-02-26 21:41:50 -0300

    codigos da aula 01

---
+++ aulas/aula01/ola_mundo.c
@@ -0,0 +1,12 @@
+/*
+ Aula01 - Esturtura basica de um programa em C
+ */
+#include <stdio.h> // inclusão de biblioteca
+
+// funcao principal que inicia o programa
+int main(){
+
+  printf("Ola pessoal!\n");
+
+    return 0;
+}
